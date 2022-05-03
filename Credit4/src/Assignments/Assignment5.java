a01e65562bee4f0b8fddcfe39d09cdd6f44c7a22
16107008 <16107008@L815WD220019.edu.cbe.ab.ca>
2022-05-03 15:14:18 -0600

    hello

--- Credit4/src/Assignments/Assignment5.java
+++ Credit4/src/Assignments/Assignment5.java
@@ -1,9 +1,20 @@
 package Assignments;
 
-public class Assignment5 {
+public class Assignment5 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
+		int num1 = 79 + 3 * (4 + 82 - 68) - 7 +19;
+
+		int num2 = (179 +21 +10) / 7 + 181;
+		
+		int num3 = 10389 * 56 * 11 + 2246;
+		
+		System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 =" +num1);
+		System.out.println("(179 + 21 + 10) / 7 + 181 =" +num2);
+		System.out.println("10389 * 56 * 11 + 2246 =" + num3);
 
 	}
 
