f6d1569d82cac143a6919dec09e2bc2c685024e3
16107008 <16107008@L815WD220019.edu.cbe.ab.ca>
2022-05-04 12:46:32 -0600

    update

--- Credit4/src/Assignments/Assignment5.java
+++ Credit4/src/Assignments/Assignment5.java
@@ -5,16 +5,16 @@ public class Assignment5
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-		int num1 = 79 + 3 * (4 + 82 - 68) - 7 +19;
+		int num1 = 79 + 3 * (4 + 82 - 68) - 7 +19;//gives the calculation a variable
 
-		int num2 = (179 +21 +10) / 7 + 181;
+		int num2 = (179 +21 +10) / 7 + 181;//gives the calculation a variable
+
+		int num3 = 10389 * 56 * 11 + 2246;//gives the calculation a variable
+
 		
-		int num3 = 10389 * 56 * 11 + 2246;
-		
-		System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 =" +num1);
-		System.out.println("(179 + 21 + 10) / 7 + 181 =" +num2);
-		System.out.println("10389 * 56 * 11 + 2246 =" + num3);
+		System.out.println("79 + 3 * (4 + 82 - 68) - 7 + 19 =" +num1);//prints the equation and num1
+		System.out.println("(179 + 21 + 10) / 7 + 181 =" +num2);//prints the equation and num2
+		System.out.println("10389 * 56 * 11 + 2246 =" + num3);//prints the equation and num3
 
 	}
 
---
+++ Credit4/src/Assignments/Assignment6.java
@@ -0,0 +1,11 @@
+package Assignments;
+
+public class Assignment6 {
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
