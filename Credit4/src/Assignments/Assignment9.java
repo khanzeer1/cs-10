fe350a6be5674d098d3f41eb4aba9c494a1de566
16107008 <16107008@L815WD220019.edu.cbe.ab.ca>
2022-05-19 15:33:54 -0600

    hello

--- Credit4/src/Assignments/Assignment9.java
+++ Credit4/src/Assignments/Assignment9.java
@@ -1,10 +1,28 @@
 package Assignments;
 
+import java.util.Scanner;
+
 public class Assignment9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Scanner userinput= new Scanner(System.in);//create scanner
+		
+		System.out.println("Enter an integer: ");//prompt integer
+	
+		int int1 = userinput.nextInt();//record integer
+	
+		System.out.println("Enter a second integer: ");//prompt 2nd integer
+	
+		int int2 = userinput.nextInt();//record 2nd integer
+	
+		System.out.println(int1 + "/" + int2 + "=" + int1/int2);//recording the values and outputting the equations
+		
+		System.out.println(int1 + "%" + int2 + "=" + int1%int2 + "\n");
+	
+		System.out.println(int2 + "/" + int1 + "=" + int2/int1);
+	
+		System.out.println(int2 + "%" + int1 + "=" + int2%int1);
 	}
 
 }
