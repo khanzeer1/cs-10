5fae071b28d9a1459db8f88bce4adb7bf9915d09
khanzeer1 <102550265+khanzeer1@users.noreply.github.com>
2022-06-05 20:37:20 -0600

    Update Assignment9.java

--- Credit4/src/Assignments/Assignment9.java
+++ Credit4/src/Assignments/Assignment9.java
@@ -2,26 +2,18 @@ package Assignments;
 
 import java.util.Scanner;
 
-public class Assignment9 {
+public class Assignment9 
+{
 
 	public static void main(String[] args) {
-
 		Scanner userinput= new Scanner(System.in);//create scanner
-		
 		System.out.println("Enter an integer: ");//prompt integer
-	
 		int int1 = userinput.nextInt();//record integer
-	
 		System.out.println("Enter a second integer: ");//prompt 2nd integer
-	
 		int int2 = userinput.nextInt();//record 2nd integer
-	
 		System.out.println(int1 + "/" + int2 + "=" + int1/int2);//recording the values and outputting the equations
-		
 		System.out.println(int1 + "%" + int2 + "=" + int1%int2 + "\n");
-	
 		System.out.println(int2 + "/" + int1 + "=" + int2/int1);
-	
 		System.out.println(int2 + "%" + int1 + "=" + int2%int1);
 	}
 
